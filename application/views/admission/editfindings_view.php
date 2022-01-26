e61fddbaaec1182e5f8b5e74ef32ce4662abcb5a
Kim <kimdiannebagasona@gmail.com>
2022-01-26 16:34:04 +0800

    Register and Update Patiend Record

--- application/views/admission/editfindings_view.php
+++ application/views/admission/editfindings_view.php
@@ -98,9 +98,9 @@
 
          <div style="margin-bottom:17px;">
         <div class="row no-gutters">
-                 <div class="text-xs font-weight-bold text-secondary text-uppercase mb-1">Civil Status</div>
+                 <div class="text-xs font-weight-bold text-secondary text-uppercase mb-1">Developmental Stage</div>
                 </div>
-        <div class="h5 mb-1 font-weight-bold text-gray-800"><?php echo $get_findings_view->pr_civilstat; ?></div>
+        <div class="h5 mb-1 font-weight-bold text-gray-800"><?php echo $get_findings_view->pr_devstage; ?></div>
          </div>
 
          <div style="margin-bottom:17px;">
@@ -126,9 +126,9 @@
 
          <div style="margin-bottom:18px;">
         <div class="row no-gutters">
-                 <div class="text-xs font-weight-bold text-secondary text-uppercase mb-1">Occupation</div>
+                 <div class="text-xs font-weight-bold text-secondary text-uppercase mb-1">Nationality</div>
                 </div>
-        <div class="h5 mb-1 font-weight-bold text-gray-800"><?php echo $get_findings_view->pr_occup; ?></div>
+        <div class="h5 mb-1 font-weight-bold text-gray-800"><?php echo $get_findings_view->pr_nationality; ?></div>
          </div>
 
          <div style="margin-bottom:18px;">
